1267319c4da7b76907d9e84f6ff8f8640e551747
Paolo <paolo.bisignano@mail.polimi.it>
2021-04-16 12:02:01 +0200

    Implemented transformWhiteMarbles method in PersonalBoard

--- src/main/java/it/polimi/ingsw/model/PersonalBoard.java
+++ src/main/java/it/polimi/ingsw/model/PersonalBoard.java
@@ -1,8 +1,16 @@
 package it.polimi.ingsw.model;
 
+import it.polimi.ingsw.model.exceptions.NotEnoughWhiteMarblesException;
+
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 
 public class PersonalBoard {
+    private HashMap<Marble,Integer> temporaryMarbles;
+    private ArrayList<PowerOfProduction> powerOfProductions;
+    private ArrayList<LeaderCard> leaderCards;
+
 
     public void activateProduction(HashMap<Resource,Integer> costStrongbox, HashMap<Resource,Integer> costWarehouseDepot, PowerOfProduction powerOfProduction){
 
@@ -12,8 +20,18 @@ public class PersonalBoard {
 
     }
 
-    public void transformWhiteMarble(LeaderCard leaderCard){
-
+    /**
+     * This method transform a number of white marbles in the temporaryMarbles map
+     * @param leaderCard is the card to use to transform marbles
+     * @param numOfTransformations is the number of marbles that needs to be transformed
+     * @throws NotEnoughWhiteMarblesException this exception is thrown when there are not enough white marbles in the given map of marbles
+     */
+    public void transformWhiteMarble(LeaderCard leaderCard, Integer numOfTransformations) throws NotEnoughWhiteMarblesException {
+        try {
+            leaderCard.abilityMarble(temporaryMarbles,numOfTransformations);
+        } catch (NotEnoughWhiteMarblesException exception) {
+            throw new NotEnoughWhiteMarblesException();
+        }
     }
 
     public void transformMarbles(){
