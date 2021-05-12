1797265a4d292cb1fef1d62d1bc66730f60b24e2
andreacerasani <andrea.cerasani@mail.polimi.it>
2021-05-12 21:38:53 +0200

    Added nickname attribute and constructor to Message class

--- src/main/java/it/polimi/ingsw/common/Message.java
+++ src/main/java/it/polimi/ingsw/common/Message.java
@@ -3,6 +3,18 @@ package it.polimi.ingsw.common;
 import it.polimi.ingsw.server.controller.Controller;
 
 public abstract class Message {
+    private final String nickname;
     MessageType messageType;
+
+    public Message(String nickname, MessageType messageType) {
+        this.messageType = messageType;
+        this.nickname = nickname;
+    }
+
+
     public abstract void handleMessage(Controller controller);
+
+    public String getNickname() {
+        return nickname;
+    }
 }
