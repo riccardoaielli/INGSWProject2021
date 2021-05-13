e0cbfddcec5c85033455dd01bce35060aa47445a
Paolo <paolo.bisignano@mail.polimi.it>
2021-05-13 23:59:37 +0200

    Added MessageDeserializer javaDoc

--- src/main/java/it/polimi/ingsw/common/MessageDeserializer.java
+++ src/main/java/it/polimi/ingsw/common/MessageDeserializer.java
@@ -1,20 +1,32 @@
 package it.polimi.ingsw.common;
 
 import com.google.gson.*;
-import it.polimi.ingsw.common.*;
-
 import java.lang.reflect.Type;
 
+/**
+ * This class offers a custom deserializer to deserialize a json string into a massage without knowing the type of the message
+ */
 public class MessageDeserializer implements JsonDeserializer{
     private GsonBuilder gsonBuilder;
     private Gson customGson;
 
+    /**
+     * This constructor creates the custom gsonBuilder to deserialize the messages
+     */
     public MessageDeserializer() {
         gsonBuilder = new GsonBuilder().registerTypeAdapter(MessageToServer.class,this);
         customGson = gsonBuilder.create();
     }
 
 
+    /**
+     * This is the custom deserializer that deserializes a massage in a specific type of massage reading its type from the json
+     * @param json is the json message to deserialize
+     * @param type is the class type to deserialize
+     * @param jsonDeserializationContext is the deserialization context
+     * @return a message that extends MessageToServer
+     * @throws JsonParseException the parsing goes wrong
+     */
     @Override
     public MessageToServer deserialize(JsonElement json, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
         JsonObject jsonObject = json.getAsJsonObject();
@@ -40,6 +52,11 @@ public class MessageDeserializer implements JsonDeserializer{
         return null;
     }
 
+    /**
+     *This method deserializes a string into a massage of a specific type that extends MessageToServer
+     * @param serializedMessage is a String with the json massage to deserialize
+     * @return a massage of a specific type that extends MessageToServer
+     */
     public MessageToServer deserializeMessage(String serializedMessage){
         return customGson.fromJson(serializedMessage, MessageToServer.class);
     }
